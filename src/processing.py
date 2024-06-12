d41a82c87bebd657fd333acead53405a3c92cf35
Gennady Ershov <ershov2007@gmail.com>
2024-06-13 00:31:58 +0800

    fixed processing.py

--- src/processing.py
+++ src/processing.py
@@ -2,12 +2,12 @@ from typing import Optional
 
 
 def filter_by_state(
-    operations: list[dict], state: Optional[str] = "EXECUTED"
+    operations: list, state: Optional[str] = "EXECUTED"
 ) -> list[dict]:
     """Фильтрует транзакции по состоянию"""
     return [elem for elem in operations if elem["state"] == state]
 
 
-def filter_by_date(operations: list[dict], ascending: bool = False) -> list[dict]:
+def filter_by_date(operations: list, ascending: bool = False) -> list:
     """Фильтрует транзакции по дате"""
     return sorted(operations, key=lambda date: date.get("date"), reverse=ascending)
--- src/widget.py
+++ src/widget.py
@@ -1,34 +1,17 @@
 from src.masks import mask_account, mask_card
 
 
-# def mask_card_and_account(acc_number: str) -> str:
-#     """Возвращет исходную строку с замаскированным номером карты/счета"""
-#     pay_system = ("Visa", "Maestro", "MasterCard")
-#     for i in acc_number.split():
-#         if i in pay_system:
-#             card_name = " ".join([i for i in acc_number.split() if i.isalpha()])
-#             card_number = mask_card(
-#                 "".join([i for i in acc_number.split() if i.isdigit()])
-#             )
-#             return f"{card_name + " " + card_number}"
-#         elif i == "Счет":
-#             account = "".join([i for i in acc_number if i.isdigit()])
-#             return f"Счет {mask_account(account)}"
-
-
-def mask_account_card(acc_number: str) -> str:
+def mask_card_and_account(acc_number: str) -> str:
     """Возвращет исходную строку с замаскированным номером карты/счета"""
-    pay_system = ("Visa", "Maestro", "MasterCard")
-    for i in acc_number.split():
-        if i in pay_system:
-            card_name = " ".join([i for i in acc_number.split() if i.isalpha()])
-            card_number = mask_card(
-                "".join([i for i in acc_number.split() if i.isdigit()])
-            )
-            return f"{card_name + " " + card_number}"
-        elif i == "Счет":
-            account = "".join([i for i in acc_number if i.isdigit()])
-            return f"Счет {mask_account(account)}"
+    if acc_number.lower().startswith("счет"):
+        account = "".join([i for i in acc_number if i.isdigit()])
+        return f"Счет {mask_account(account)}"
+    else:
+        card_name = " ".join([i for i in acc_number.split() if i.isalpha()])
+        card_number = mask_card(
+            "".join([i for i in acc_number.split() if i.isdigit()])
+        )
+        return f"{card_name + " " + card_number}"
 
 
 def get_data (date_and_time: str) -> str:
@@ -38,4 +21,5 @@ def get_data (date_and_time: str) -> str:
 
 
 if __name__ == "__main__":
-    print(mask_account_card("Visa Platinum 7000 7922 8960 6361"))
+    print(mask_card_and_account("Счет 73654108430135874305"))
+    print(mask_card_and_account("Visa Platinum 7000 7922 8960 6361"))
